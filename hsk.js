5305b3c671570aef06446a6eb1652abce1a76b6f
peter <im7mortal@gmail.com>
2014-04-20 13:17:13 +0400

    :banny:

--- hsk.js
+++ hsk.js
@@ -8,39 +8,26 @@
 
 
 $(document).ready(function(){
-    $("#create").click( function(){
+
+     function lol(){
         $.getJSON('hsk_words.json', function(hsk_words) {
-            var output="<table class=\"table table-bordered\">";
-            output+="<thead><tr class=\"color number\"><th class='char'>char</th><th class='pinyin'>pinyin</th><th class='russian'>russian</th><th class='english'>english</th></tr></thead>";
-            output+="<tbody>";
+            var output="";
+
 
             for (var i in hsk_words.words) {
-                output+="<tr class=\'"  + hsk_words.words[i].part_of_speech+ "  "+ hsk_words.words[i].meaning +"\'>" +"<td class=\"char   char_big\">" +hsk_words.words[i].char + "</td>"+" "+"<td class=\"pinyin  \">" + hsk_words.words[i].pinyin +"</td>"+" "+"<td class=\"russian\">" + hsk_words.words[i].russian +"</td>"+" "+"<td class=\"english\">" + hsk_words.words[i].english +"</td>"+ "</tr>";
+                output+="<tr class=\'"  + hsk_words.words[i].part_of_speech+ "  "+ hsk_words.words[i].meaning +"\'>" +"<td class=\"char   char_big\">" +hsk_words.words[i].char + "</td>"+" "+"<td class=\"pinyin  \">" + hsk_words.words[i].pinyin +"<span class=\"glyphicon glyphicon-play\"></span></td>"+" "+"<td class=\"russian\">" + hsk_words.words[i].russian +"</td>"+" "+"<td class=\"english\">" + hsk_words.words[i].english +"</td>"+ "</tr>";
             }
-            output+="</tbody>";
-            output+="</table>";
-
             document.getElementById("place").innerHTML=output;
             return false;
         });
         return false;
+    };
+lol();
+
+    $("#create").click(function (){
+        $("*").show();
+        return false;
     });
-
-//    $(document).ready(function(){
-//        $("#create").click( function(){
-//            $.getJSON('hsk_words.json', function(hsk_words) {
-//                var output="<ul>";
-//                for (var i in hsk_words.words) {
-//                    output+="<li class=\'" + hsk_words.words[i].part_of_speech+ "  "+ hsk_words.words[i].meaning +"\'>" +"<span class=\"char\">" +hsk_words.words[i].char + "</span>"+" "+"<span class=\"pinyin\">" + hsk_words.words[i].pinyin +"</span>"+" "+"<span class=\"russian\">" + hsk_words.words[i].russian +"</span>"+" "+"<span class=\"english\">" + hsk_words.words[i].english +"</span>"+ "</li>";
-//                }
-//                output+="</ul>";
-//
-//                document.getElementById("place").innerHTML=output;
-//                return false;
-//            });
-//            return false;
-//        });
-
     $("#color").click(function (){
         $("tr:not(.color)").toggle();$("tr.color").show();
         return false;
@@ -68,4 +55,5 @@ $(document).ready(function(){
     });
 
 
-});
+}
+);
--- random.js
+++ random.js
@@ -1,36 +1,71 @@
 "use strict";
 
 $(document).ready(function(){
-    $("#next").click( function(){
-        $.getJSON('hsk_words.json', function(hsk_words) {
-           var Q = (Math.floor(Math.random() * (19 - 0 + 1)) + 0);
-            var output="<span>"+ hsk_words.words[Q].char  +"</span></br>";
-            var a = [Q,Math.floor(Math.random() * (19 - 0 + 1)) + 0,Math.floor(Math.random() * (19 - 0 + 1)) + 0,Math.floor(Math.random() * (19 - 0 + 1)) + 0];
 
-            for ( var i = a.length; i-->0; ) {
-                var t = a[i],
-                    j = Math.floor(i*Math.random());
-                a[i] = a[j];
-                a[j] = t;
-            }
-            var b = a;
+//    TEST
+    $.getJSON('hsk_words.json', function(hsk_words) {
 
+//        RANDOM CHAR
 
+        var question = (Math.floor(Math.random() * (19 - 0 + 1)) + 0);
+        var output="<span>"+ hsk_words.words[question].char  +"</span></br>";
+        var new_random = [question,Math.floor(Math.random() * (19 - 0 + 1)) + 0,Math.floor(Math.random() * (19 - 0 + 1)) + 0,Math.floor(Math.random() * (19 - 0 + 1)) + 0];
 
-
-
+        for ( var i = new_random.length; i-->0; ) {
+            var t = new_random[i],
+                j = Math.floor(i*Math.random());
+            new_random[i] = new_random[j];
+            new_random[j] = t;
+        }
 
 
 
+        document.getElementById("place").innerHTML=output;
 
 
-            output +="<span>"+ hsk_words.words[b[0]].russian  +"</span></br>";
-           output +="<span>"+ hsk_words.words[b[1]].russian  +"</span></br>";
-           output +="<span>"+ hsk_words.words[b[2]].russian  +"</span></br>";
-           output +="<span>"+ hsk_words.words[b[3]].russian  +"</span></br>";
-            document.getElementById("place").innerHTML=output;
-            return false;
-        });
+
+
+
+        var  first_var =""+ hsk_words.words[new_random[0]].russian+"";
+        document.getElementById("first_var").innerHTML=first_var;
+
+        var first_ansver =""+ hsk_words.words[new_random[0]].char+"  "+ hsk_words.words[new_random[0]].pinyin+"  ";
+//         $("#first_content").css('background-color' , '#ddd');
+
+        document.getElementById("first_ansver").innerHTML=first_ansver;
+
+
+
+
+        var second_var =hsk_words.words[new_random[1]].russian;
+        document.getElementById("second_var").innerHTML=second_var;
+        var second_ansver =""+ hsk_words.words[new_random[1]].char+"  "+ hsk_words.words[new_random[1]].pinyin+"  ";
+
+        document.getElementById("second_ansver").innerHTML=second_ansver;
+
+        var  third_var = hsk_words.words[new_random[2]].russian ;
+        document.getElementById("third_var").innerHTML=third_var;
+        var third_ansver =""+ hsk_words.words[new_random[2]].char+"  "+ hsk_words.words[new_random[2]].pinyin+"  ";
+
+        document.getElementById("third_ansver").innerHTML=third_ansver;
+
+        var fourth_var = hsk_words.words[new_random[3]].russian ;
+        document.getElementById("fourth_var").innerHTML=fourth_var;
+        var fourth_ansver =""+ hsk_words.words[new_random[3]].char+"  "+ hsk_words.words[new_random[3]].pinyin+"  ";
+
+        document.getElementById("fourth_ansver").innerHTML=fourth_ansver;
+
         return false;
     });
+    return false;
+
+
+
+//    $("#next").click( function(){
+//    });
+//
+//    function ansverTrue(){
+//        if ()
+//    };
+
 })
