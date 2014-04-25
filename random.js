6a48e372a60e5cd5d88bcb3ba81063daf78cf276
peter <im7mortal@gmail.com>
2014-04-25 23:13:07 +0400

    global

--- random.js
+++ random.js
@@ -1,29 +1,19 @@
 "use strict";
 
 $(document).ready(function(){
-//    TEST
-
-   fill();
-
-
-
+    fill();
 
   $("#next").click(function (){
       $("div.content").css("display","none");
       fill();
   });
 
-
-
     function fill(){
-
     $.getJSON('hsk_words.json', function(hsk_words) {
 
 //        RANDOM CHAR
-
         var question = (Math.floor(Math.random() * (19 - 0 + 1)) + 0);
-        var output=hsk_words.words[question].char;
-        document.getElementById("place").innerHTML=output;
+        document.getElementById("random").innerHTML=hsk_words.words[question].char;
 
 //     check на одинаковые члены
         var test_random = (function (){
@@ -31,20 +21,16 @@ $(document).ready(function(){
         Math.floor(Math.random() * (19 - 0 + 1)) + 0,
                   Math.floor(Math.random() * (19 - 0 + 1)) + 0,
                   Math.floor(Math.random() * (19 - 0 + 1)) + 0];
-            for(var i=0; i<3; i++) {
-                for(var j=0; j<3; j++) {
+            for(var i=0; i<4; i++) {
+                for(var j=0; j<4; j++) {
                    if(i==j){
-
                    }else if (test_random[i]==test_random[j]){
                        test_random[i]= Math.floor(Math.random() * (19 - 0 + 1)) + 0;
                    }else  ;
                 }
             }
-
             return test_random;
         }) ();
-
-
 
         for ( var i = test_random.length; i-->0; ) {
             var t = test_random[i],
@@ -72,15 +58,8 @@ var pinyin_var= ["first_ansver","second_ansver","third_ansver","fourth_ansver"];
 for(var i=0; i<4;i++){
     document.getElementById(pinyin_var[i]).innerHTML=hsk_words.words[test_random[i]].pinyin;
 }
-
-
-
         return false;
     });
      return false;}
-
-
-
-
 
 })
