c21e057443e1cf06e06b2d80d7892e914547a539
peter <im7mortal@gmail.com>
2014-05-04 09:18:38 +0400

    repair

---
+++ engine.js
@@ -0,0 +1,23 @@
+"use strict";
+var json;
+var color_bool= true;
+var number_bool= true;
+
+$.getJSON('hsk_words.json', function(hsk_words) {
+    json=hsk_words;
+});
+
+//$("#color_var").change(function (){
+//
+//    color_bool = !!$("#color_var").prop('checked');
+//    console.log(color_bool);
+//    return false;
+//});
+//$("#number_var").change(function (){
+//
+//    number_bool = !!$("#cnumber_var").prop('checked');
+//    console.log(color_bool);
+//    return false;
+//});
+
+
--- random.js
+++ random.js
@@ -1,18 +1,55 @@
 "use strict";
 var question;
+var length_words;
 var arr=new Array(10);
-fill();
+var color_bool= true;
+var number_bool= true;
+
+$("#color_var").change(function (){
+
+    color_bool = !!$("#color_var").prop('checked');
+    console.log(color_bool);
+    return false;
+});
+$("#number_var").change(function (){
+
+    number_bool = !!$("#cnumber_var").prop('checked');
+    console.log(color_bool);
+    return false;
+});
+
+
+choose_machine();
+
+
+function choose_machine(){
+    if(color_bool&&number_bool){fill();}else{
+    var light=[];
+
+    }
+
+}
+
+
+
+
 
 $("#next").click(function (){
-fill();
+    choose_machine();
 });
 
 $("#random").click(function (){
     document.getElementById('sound').play();
     return false;
 });
-var length_words;
 
+function main_char(json){
+    var length=json.words[question].char.length;
+    if(length==1){$("#random").css("width",92)}else{
+        $("#random").css("width",80*length)
+    }
+    document.getElementById("random").innerHTML=json.words[question].char;
+}
 
 function randomize(){
     question = random_var();
@@ -32,32 +69,32 @@ function random_var(){
     return Math.floor(Math.random() * length_words);
 }
 
+
+
+function music(json){
+    document.getElementById("music").innerHTML= "<audio id=\"sound\" src=\"" + json.words[question].sound +"\" autoplay></audio>";
+    return function(){};
+}
+
 function fill(){
     $("div.content").css("display","none").css("border","");
     $.getJSON('hsk_words.json', function(hsk_words) {
         length_words = hsk_words.words.length-1;
         randomize();
-        main_char();
-        generate_var();
-        music();
-
-
-
-
-function music(){
-    document.getElementById("music").innerHTML= "<audio id=\"sound\" src=\"" + hsk_words.words[question].sound +"\" autoplay></audio>";
-    return function(){};
-   }
-
-function main_char(){
-    var length=hsk_words.words[question].char.length;
-    if(length==1){$("#random").css("width",92)}else{
-        $("#random").css("width",80*length)
-    }
-    document.getElementById("random").innerHTML=hsk_words.words[question].char;
+        main_char(hsk_words);
+        generate_var(hsk_words);
+        music(hsk_words);
+        $("div.content:has(button.primary)").css("border","2px solid green");
+        $("div.content:has(button.danger)").css("border","2px solid red");
+        return false;
+    });
+    return false;
 }
 
-function generate_var(){
+
+
+
+function generate_var(json){
     var test_random = (function (){
         var test_random = [question,random_var(),random_var(),random_var()];
         for(var i=0; i<4; i++) {
@@ -86,39 +123,28 @@ function generate_var(){
 
     var russian_var= ["first_var","second_var","third_var","fourth_var"];
     for(var i=0; i<4;i++){
-        document.getElementById(russian_var[i]).innerHTML=hsk_words.words[test_random[i]].russian;
+        document.getElementById(russian_var[i]).innerHTML=json.words[test_random[i]].russian;
     }
     var char_var= ["first_ansver_char","second_ansver_char","third_ansver_char","fourth_ansver_char"];
     for(var i=0; i<4;i++){
-        document.getElementById(char_var[i]).innerHTML=hsk_words.words[test_random[i]].char;
+        document.getElementById(char_var[i]).innerHTML=json.words[test_random[i]].char;
     }
     var pinyin_var= ["first_ansver","second_ansver","third_ansver","fourth_ansver"];
     for(var i=0; i<4;i++){
-        document.getElementById(pinyin_var[i]).innerHTML=hsk_words.words[test_random[i]].pinyin;
+        document.getElementById(pinyin_var[i]).innerHTML=json.words[test_random[i]].pinyin;
         if (test_random[i] == question){
             document.getElementById("true_"+pinyin_var[i]).innerHTML="<button class=\"primary next\">Правильно!!</button>";
             $(".next").click(function (){
-                fill();
+                choose_machine();
             });
 
         } else {document.getElementById("true_"+pinyin_var[i]).innerHTML="<button class=\"danger next\">Попробуй ещё!!</button>";
             $(".next").click(function (){
-                fill();
+                choose_machine();
             });
         }
     }
 }
-
-
-        $("div.content:has(button.primary)").css("border","2px solid green");
-        $("div.content:has(button.danger)").css("border","2px solid red");
-        return false;
-    });
-    return false;
-}
-
-
-
 
 
 
