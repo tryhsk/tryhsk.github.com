093e84d6b56ec991c05ad67b432b387a88c766e3
peter <im7mortal@gmail.com>
2014-05-05 22:36:00 +0400

    idea

--- engine.js
+++ engine.js
@@ -1,23 +1,23 @@
 "use strict";
 var json;
-var color_bool= true;
-var number_bool= true;
-// nak
-$.getJSON('hsk_words.json', function(hsk_words) {
-    json=hsk_words;
-});
+var noun_bool= true;
+var pronoun_bool= true;
+var verb_bool= true;
+var subject_bool= true;
 
-//$("#color_var").change(function (){
-//
-//    color_bool = !!$("#color_var").prop('checked');
-//    console.log(color_bool);
-//    return false;
-//});
-//$("#number_var").change(function (){
-//
-//    number_bool = !!$("#cnumber_var").prop('checked');
-//    console.log(color_bool);
-//    return false;
-//});
+var test=[];
+
+$.getJSON('words.json', function(hsk_words) {
+
+    if(noun_bool){
+        for(var i;i<test.length;i++){
+
+        }
+    }
+
+
+
+
+});
 
 
