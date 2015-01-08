10f12f6eee21d5700c4edef3f54eb512b022bc7d
im7mortal <im7mortal@gmail.com>
2015-01-08 22:28:17 +0200

    reduce code

--- partials/checkbox/CheckboxController.js
+++ partials/checkbox/CheckboxController.js
@@ -1,21 +1,6 @@
 tryHskControllers.controller('CheckboxController', function ($scope, $rootScope, checkboxValues) {
-
-	//TODO хрень какая то
 	$rootScope.checkboxValues = checkboxValues.getCheckboxValues();
-	$scope.checkboxValues = $rootScope.checkboxValues;
-	$scope.count = 0;
-	$scope.$watch('checkboxValues', function () {
-		checkboxValues.refreshCheckboxValues($scope.checkboxValues);
-		$rootScope.checkboxValues = $scope.checkboxValues;
-		try {
-			// init
-			if ($scope.count > 1) $scope.$parent.refresh();
-			$scope.count = ++$scope.count;
-		} catch (e) {
-		}
-	}, true);
 	$rootScope.$watch('checkboxValues', function () {
-		$scope.checkboxValues = $rootScope.checkboxValues;
+		checkboxValues.refreshCheckboxValues($scope.checkboxValues);
 	}, true);
-// 3 аргумент true важен в $watch так как из за него наблюдается весь обьект целиком
 });
